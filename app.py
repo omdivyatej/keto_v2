2d555ffd67c8de9579f5b6ba35316662081d7edb
Om Divyatej <od302002@gmail.com>
2021-12-05 23:09:26 +0530

    First initialization

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, app,render_template,url_for,Response
+from flask.sessions import NullSession
+import pose_Detection,cr1
+app= Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/feed')
+def open_camera():
+    return Response(pose_Detection.running(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+if __name__ == '__main__':    
+    app.run(debug=True)
+
+    
+
---
+++ cr1.py
@@ -0,0 +1,2 @@
+def adding():
+    return '2'
